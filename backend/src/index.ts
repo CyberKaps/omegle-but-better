25d7b61fa99941aa4c0902cc43fe8ab71291d58d
Kalpesh Borde <bordekalpesh@gmail.com>
2025-05-16 04:27:35 +0530

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,11 @@ import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(http);
 
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
 
 
 io.on('connection', (socket: Socket) => {
