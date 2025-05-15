df384af8bfc65f6a8223e3e15811de92ec079dc4
Kalpesh Borde <bordekalpesh@gmail.com>
2025-05-16 02:17:15 +0530

    basic component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
+
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import './App.css'
+import { Landing } from './components/Landing'
+import { Room } from './components/Room'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' component={<Landing />} />
+        <Route path='/room' element={<Room />} />
+        <Route path='/' element={<Landing />} />
       </Routes>
     </BrowserRouter>
   )
