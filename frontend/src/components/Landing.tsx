ab4339ea9e1a36b346e498b30ebf55bdae61f020
Kalpesh Borde <bordekalpesh@gmail.com>
2025-05-16 02:34:03 +0530

    input name as query param

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,16 +1,14 @@
 import { useState } from "react"
+import { Link } from "react-router-dom";
 
 
 export const Landing = () => {
-
     const [name, setName] = useState("");
 
     return <div>
         <input onChange={(e) => {
             setName(e.target.value)
         }} type="text" />
-        <button onClick={() => {
-            // join room logic here
-        }}>Join</button>
+        <Link to={`/room/?name=${name}`}>Join</Link>
     </div>
 }
